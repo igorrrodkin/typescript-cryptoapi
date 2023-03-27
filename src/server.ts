b3d918d0710fa14b5ba194cf3dce497f8a70b80f
Igor Rodkin <igorrodkin@Igors-MacBook-Air.local>
2023-03-27 18:57:55 +0300

    ab

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,18 @@ import express from "express";
 import cron from "node-cron";
 import { loadContent } from "./utils/load.js";
 import router from "./routes/cryptoRoutes.js";
-import dbConn from "./pgConnect.js";
+// import dbConn from "./pgConnect.js";
 import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 app.use("/", router);
 
-app.listen(process.env.PORT_APP, () => {
-  console.log(`App is running on port ${process.env.PORT_APP}`);
+app.listen(2400, () => {
+  console.log(`App is running on port ${2400}`);
   cron.schedule("*/5 * * * *", (): void => {
-    const client = dbConn();
-    loadContent(client);
+    // const client = dbConn();
+    // loadContent(client);
     console.log("New content loaded", new Date());
   });
 });
